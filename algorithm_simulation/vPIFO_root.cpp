9bd4aab8b5c218207722db23bcac86aaf9cda2bc
Ruoshi Sun <19307130296@fudan.edu.cn>
2023-09-13 17:01:15 +0800

    Update vPIFO_root.cpp
    more specific variable name

--- algorithm_simulation/vPIFO_root.cpp
+++ algorithm_simulation/vPIFO_root.cpp
@@ -17,16 +17,16 @@ enum Type {
 struct Task {
     enum Type type;
     int root, TTL, val;
-    Task() {
+    Task () {
         TTL = 0;
         type = Empty;
     }
-    Task(enum Type t, int r, int ttl) {
+    Task (enum Type t, int r, int ttl) {
         type = t;
         root = r;
         TTL = ttl;
     }
-}r[N];
+}RPU[N];
 
 struct Tree {
     int min[2], num[2], son[2];
@@ -35,65 +35,65 @@ struct Tree {
         num[0] = num[1] = 0;
         son[0] = son[1] = 0;
     }
-}node[M<<N];
+} node[M<<N];
 
 // record the root node of trees
 int tree[M], tot;
 
-int get_son(int r, int d) {
-    if (node[r].son[d] != 0)
-        return node[r].son[d];
+int get_son (int root, int d) {
+    if (node[root].son[d] != 0)
+        return node[root].son[d];
     tot++;
-    node[r].son[d] = tot;
+    node[root].son[d] = tot;
     return tot;
 }
 
-void push(int r, int x) {
+void push (int root, int val) {
     // try to insert x in this node
-    if (node[r].min[0] == inf) {
-        node[r].min[0] = x;
+    if (node[root].min[0] == inf) {
+        node[root].min[0] = val;
         return;
     }
-    if (node[r].min[1] == inf) {
-        node[r].min[1] = x;
+    if (node[root].min[1] == inf) {
+        node[root].min[1] = val;
         return;
     }
 
     // insert x in one of the son
     int go_down = 0;
-    if (node[r].num[0] > node[r].num[1])
+    if (node[root].num[0] > node[root].num[1])
         go_down = 1;
-    if (x < node[r].min[go_down]) {
-        push(get_son(r, go_down), node[r].min[go_down]);
-        node[r].min[go_down] = x;
+    if (val < node[root].min[go_down]) {
+        push(get_son(root, go_down), node[root].min[go_down]);
+        node[root].min[go_down] = val;
     }
     else
-        push(get_son(r, go_down), x);
-    node[r].num[go_down]++;
+        push(get_son(root, go_down), val);
+    node[root].num[go_down]++;
 }
 
-int pop(int r) {
+int pop (int root) {
     int ret = 0;
     int go_down = 0;
-    if (node[r].min[0] > node[r].min[1])
+    if (node[root].min[0] > node[root].min[1])
         go_down = 1;
-    ret = node[r].min[go_down];
-    node[r].min[go_down] = inf;
-    if (node[r].son[go_down] != 0) {
-        node[r].min[go_down] = pop(node[r].son[go_down]);
-        node[r].num[go_down]--;
+    ret = node[root].min[go_down];
+    node[root].min[go_down] = inf;
+    if (node[root].son[go_down] != 0) {
+        node[root].min[go_down] = pop(node[root].son[go_down]);
+        node[root].num[go_down]--;
     }
     return ret;
 }
 
-void tree_print(int fa, int r, int dep) {
-    printf("node %d is son of %d, dep = %d\n", r, fa, dep);
-    printf("min0 = %d, min1 = %d\n", node[r].min[0], node[r].min[1]);
-    printf("num0 = %d, num1 = %d\n", node[r].num[0], node[r].num[1]);
-    if (node[r].son[0])
-        tree_print(r, node[r].son[0], dep + 1);
-    if (node[r].son[1])
-        tree_print(r, node[r].son[1], dep + 1);
+void tree_print (int fa, int root, int dep) {
+    printf("node %d is son of %d, dep = %d\n", root, fa, dep);
+    printf("min0 = %d, min1 = %d\n", node[root].min[0], node[root].min[1]);
+    printf("num0 = %d, num1 = %d\n", node[root].num[0], node[root].num[1]);
+    if (node[root].son[0])
+        tree_print(root, node[root].son[0], dep + 1);
+    if (node[root].son[1])
+        tree_print(root, node[root].son[1], dep + 1);
 }
 
 queue<Task> task_list[N];
@@ -145,10 +145,10 @@ int main() {
         // statistics before updating status
         for (int i = 0; i < N; ++i) {
             // the RPU is running
-            if (r[i].type == Push || r[i].type == Pop || r[i].type == WriteBack)
+            if (RPU[i].type == Push || RPU[i].type == Pop || RPU[i].type == WriteBack)
                 use_num++;
-            // finish a root pop, now we know which tree to pop
-            if (r[i].TTL == 1 && r[i].type == WriteBack && r[i].root == 0) {
+            // after a root pop, now we know which tree to pop
+            if (RPU[i].TTL == N && RPU[i].type == WriteBack && RPU[i].root == 0) {
                 int ans = root_queue.front();
                 root_queue.pop();
                 t = (Task){Pop, ans, N};
@@ -159,35 +159,35 @@ int main() {
 
         // pass down the existing task
         Task nt[2]; // scrolling array used to pass the task
-        nt[1] = r[N - 1];
+        nt[1] = RPU[N - 1];
         nt[1].TTL--;
         for (int i = 0, j; i < N; ++i) {
             j = i & 1;
             // pass task in RPUi to the next RPU
-            nt[j] = r[i];
+            nt[j] = RPU[i];
             nt[j].TTL--;
             // after Pop, set a WriteBack, the TTL remains the same
-            if (r[i].type == Pop) {
-                r[i].type = WriteBack;
+            if (RPU[i].type == Pop) {
+                RPU[i].type = WriteBack;
             }
             // inherit task from the last RPU
             else if (nt[j ^ 1].TTL > 0) {
-                r[i] = nt[j ^ 1];
+                RPU[i] = nt[j ^ 1];
             }
             // set the RPU as Empty
             else {
-                r[i].type = Empty;
-                r[i].TTL = 0;
+                RPU[i].type = Empty;
+                RPU[i].TTL = 0;
             }
         }
 
         // send new task
         for (int i = 0, j; i < N; ++i) {
-            if (r[i].type == Empty) {
+            if (RPU[i].type == Empty) {
                 if (!task_list[i].empty()) {
                     Task t = task_list[i].front();
                     if (t.type == Push) {
-                        r[i] = t;
+                        RPU[i] = t;
                         if (t.root != 0)
                             push(tree[t.root], t.val);
                         else 
@@ -197,16 +197,16 @@ int main() {
                     }
                     else {
                         j = (i - 1 + N) % N;
-                        if (r[j].TTL <= 1) {
-                            r[i] = t;
+                        if (RPU[j].TTL <= 1) {
+                            RPU[i] = t;
                             task_list[i].pop();
                             if (i != 0)
                                 pop(tree[t.root]);
                             printf("log: pop tree %d in RPU %d\n", t.root, i);
                             // The last RPU should be locked
-                            if (r[j].type == Empty)
-                                r[j].type = Locked;
-                            r[j].TTL = 1;
+                            if (RPU[j].type == Empty)
+                                RPU[j].type = Locked;
+                            RPU[j].TTL = 1;
                         }
                     }
                 }
